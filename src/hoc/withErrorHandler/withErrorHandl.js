add170dbe54d12c875a88b7cf3c5524dc7b03709
djordjereact <djole07mihailovic@gmail.com>
2020-03-18 21:00:36 +0100

    Changed Error handler to handl

--- src/hoc/withErrorHandler/withErrorHandl.js
+++ src/hoc/withErrorHandler/withErrorHandl.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Modal from '../../components/UI/Modal/Modal';
-import Aux from '../Auxiliary/Auxiliary';
+import Aux from '../Auxilliary/Auxiliary';
 
 const withErrorHandler = ( WrappedComponent, axios ) => {
     return class extends Component {
