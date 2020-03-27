06d789f113f75b8a0403f0d9876576adced57633
djordjereact <djole07mihailovic@gmail.com>
2020-03-27 21:20:17 +0100

    Adding redux to my app

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -41,7 +41,9 @@ class Checkout extends Component {
                     checkoutContinued={this.checkoutContinuedHandler}/>
                 <Route
                     path={this.props.match.path + '/contact-data'}
-                    render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props}/>)} />
+                    render={(props) => (<ContactData
+                        ingredients={this.state.ingredients}
+                        price={this.state.totalPrice} {...props}/>)} />
             </div>
         );
     }
